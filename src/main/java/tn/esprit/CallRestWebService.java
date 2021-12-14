f304d765f18b28f6d495d30d5c1c04f851e4ed13
Houssem Kacem <houssemkacem@yahoo.fr>
2021-12-14 22:27:17 +0100

    delt with confilct when mergin hedi + houssems master

--- src/main/java/tn/esprit/CallRestWebService.java
+++ src/main/java/tn/esprit/CallRestWebService.java
@@ -9,9 +9,13 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
+//<<<<<<< HEAD
 // Hedi's branch
 // 2nd token
 
+//=======
+//Branch houssem
+//>>>>>>> houssem
 public class CallRestWebService {
     public static final String endpoint = "http://ip-api.com/json";
     //public static final String endpoint = "https://httpbin.org/get";
