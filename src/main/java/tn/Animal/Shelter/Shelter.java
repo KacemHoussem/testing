70dc60b7983574198eba0e053e97ad5a25264f21
Hichem <ayari.hicham01@gmail.com>
2021-12-15 13:46:30 +0000

    creation class shelter + attributs

---
+++ src/main/java/tn/Animal/Shelter/Shelter.java
@@ -0,0 +1,11 @@
+package tn.Animal.Shelter;
+
+import java.util.List;
+
+public class Shelter {
+
+	//creation Class +attributs by iheb
+	private String name ;
+	private int id;
+	private List<Animal>lstAnimal;
+}
