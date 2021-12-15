ea39920f45ca450f12cdd79c9b29543d29161a05
Houssem Kacem <houssemkacem@yahoo.fr>
2021-12-15 14:17:56 +0100

    created animal class

---
+++ src/main/java/tn/Animal/Shelter/Animal.java
@@ -0,0 +1,59 @@
+package tn.Animal.Shelter;
+
+public class Animal {
+    private int id;
+    private String name;
+    private String Type;
+    private String gender;
+    private int age;
+    private boolean vaccinated;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isVaccinated() {
+        return vaccinated;
+    }
+
+    public void setVaccinated(boolean vaccinated) {
+        this.vaccinated = vaccinated;
+    }
+
+}
