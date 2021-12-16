9e2c16993111e159c9026af0f5be3f765fc63bbd
Hedi Kamoun <hedikamounadd@hotmail.fr>
2021-12-16 14:32:45 +0100

    modify sout main

--- src/main/java/tn/Animal/Main/Main.java
+++ src/main/java/tn/Animal/Main/Main.java
@@ -3,6 +3,6 @@ package tn.Animal.Main;
 public class Main {
     public static void main(String[] args) {
         //TODO for hedi : complete method
-        System.out.println("i am hedi");
+        System.out.println("conflict");
     }
 }
