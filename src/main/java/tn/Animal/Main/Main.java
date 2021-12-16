ae36a9f3a5b2ea858a6a8a2ba6f767c90595ad05
Houssem Kacem <houssemkacem@yahoo.fr>
2021-12-16 14:25:17 +0100

    implemented main sout

--- src/main/java/tn/Animal/Main/Main.java
+++ src/main/java/tn/Animal/Main/Main.java
@@ -3,5 +3,6 @@ package tn.Animal.Main;
 public class Main {
     public static void main(String[] args) {
         //TODO for hedi : complete method
+        System.out.println("i am houssem");
     }
 }
